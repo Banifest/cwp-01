6294e310ad65de465b81b4bb948dc4443bdbccdf
banifest <banifest@gmail.com>
2017-09-12 12:56:07 +0300

    print all command line arguments

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log('Hello World');
 const name = process.argv[2];
 
 console.log(`Hi ${name}!`);
+
+for(let i = 2; i<process.argv.length; i++)
+{
+    console.log(` ${process.argv[i]} `);
+}
