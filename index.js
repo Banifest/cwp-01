319a116c8b25925576058838d59969908b7978f8
banifest <banifest@gmail.com>
2017-09-12 21:27:23 +0300

    output all files and folders in dir

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
+const path = require("path")
 
 let name = process.argv[2];
 
 let file = fs.writeFileSync(`${name}/summary.js`);
 
+console.log(
+    fs.readdir(name, (err, files) => {
+        files.forEach(file => {
+            console.log(file);
+        });
+    })
+);
