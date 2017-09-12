fc2585b493a16eeb23c7e506fe64df878e32cacd
banifest <banifest@gmail.com>
2017-09-12 22:30:24 +0300

    output all files and files in folders in dir

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const path = require("path")
 
 let name = process.argv[2];
 
-let file = fs.writeFileSync(`${name}/summary.js`);
+let file = fs.writeFileSync(`${name}/summary.js`,'a');
 
 console.log(
+
     fs.readdir(name, (err, files) => {
         files.forEach(file => {
             console.log(file);
+            let str = name + '/' + file;
+            fs.stat(str, function(err, stats){
+                if(stats.isDirectory())
+                {
+                    fs.readdir(str, function (err, deepFiles) {
+                        deepFiles.forEach(deepFile => {
+                            console.log(file + '/' + deepFile);
+                        })
+                    })
+                }
+            })
         });
     })
 );
